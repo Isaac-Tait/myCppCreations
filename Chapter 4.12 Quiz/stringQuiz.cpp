70e05f2d745c123ef8c417a8135a5ddd796a4b5b
Isaac Tait <isaac@mountaintopcoding.com>
2021-08-05 06:27:30 -0700

    add solution/fix syntax errors

--- Chapter 4.12 Quiz/stringQuiz.cpp
+++ Chapter 4.12 Quiz/stringQuiz.cpp
@@ -10,9 +10,14 @@ int main() {
 	int age{};
 	std::cin >> age;
 	
+	//My stab at the logic...
 	std::cout << "Your name is: " << name << '\n';
 	std::cout << "Your age is: " << age << '\n';
-	std::cout << "You have lived " age / name.length() << " years for each letter in your name.";
-	
+	std::cout << "You have lived " << age / name.length() << " years for each letter in your name.";
+	//The solution 
+	int letters{ static_cast<int>(name.length()) }; // get number of letters in name (including spaces)
+    double agePerLetter{ static_cast<double>(age) / letters }; // static cast age to double to avoid integer division
+    std::cout << "You've lived " << agePerLetter << " years for each letter in your name.\n";
+    
 	return 0;
 }
