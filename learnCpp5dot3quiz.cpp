968daffe08dbc99488662c7af2d6e2484b5e5b12
Isaac Tait <isaac@mountaintopcoding.com>
2021-08-06 14:58:03 -0700

    add quiz solution chapter 5.3

---
+++ learnCpp5dot3quiz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int x{};
+	
+	std::cout << "Enter an integer: " << x << '\n;
+	std::cin >> x;
+	
+	if ((x % 2) == 0) {
+		std::cout << x << " is even\n";
+	} else 
+	std::cout << x << " is odd\n";
+	
+	return 0;
+}
